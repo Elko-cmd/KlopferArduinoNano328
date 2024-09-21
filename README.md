9e5bfea027e40444fa6eed6a327bb849a8247bcd
Elko-cmd <elko.braas@gmail.com>
2024-09-21 19:08:40 +0200

    Readme + GlockenKapazität

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# Project Klopfer
-Project Klopfer consists of a 5v power supply and a 5vs olonoid, a Mosfet Driver Board and a Arduino Nano 328. The Arduino Nano is powered by the 5v power supply. 
+
+**Project Klopfer**
+=======================
+
+This Arduino code simulates the firing sequence of an AK47 rifle. The code is configured to mimic the rifle's firing frequency and magazine capacity. 
+
+**Configuration**
+---------------
+
+The code includes several configurable variables:
+
+* `KloppferPin`: The Arduino pin connected to the firing mechanism (set to 2).
+* `magazinCap`: The magazine capacity (set to 40 rounds).
+* `magazinWechsel`: The time it takes to change the magazine (set to 5 seconds or 5000 milliseconds).
+* `interval`: The firing frequency (set to 100 milliseconds, resulting in 10 shots per second).
+
+**Functionality**
+----------------
+
+The code uses the `setup()` function to initialize the firing pin as an output.
+
+The `loop()` function runs continuously, simulating the firing sequence:
+
+1. If the shot counter (`schuss`) is less than or equal to the magazine capacity, the code:
+	* Sets the firing pin high for the configured interval.
+	* Delays for 20 milliseconds.
+	* Sets the firing pin low.
+	* Increments the shot counter.
+2. If the shot counter exceeds the magazine capacity, the code:
+	* Resets the shot counter to 0.
+	* Delays for the configured magazine change time.
+
--- src/main.cpp
+++ src/main.cpp
@@ -1,36 +1,53 @@
 #include <Arduino.h>
 // Die AK47 hat eine Schussfrequenz von 600 Schuss pro Minute. Das sind 10 Schuss pro Sekunde. Also alle 50ms ein Schuss. Die MAgazinggröße der AK47 variiert zwischen 10, 20, 25, 30, 40 und 75, 100 Patronen.
 
-//CONFIG START ###########################################################
-// Arduino Pins
-  int KloppferPin = 2;
+// CONFIG START ###########################################################
+//  Arduino Pins
+int KloppferPin = 2;
 
 // Magazin Kapazitaet
-  int magazinCap = 40;
-//MAgazinwechsel Zeit in Millisekunden (1000 = 1 Sekunde)
- unsigned int magazinWechsel = 5000;
-//Schussfrequenz in Millisekunden (1000 = 1 Sekunde)
-  const int interval = 100; // 10 mal in der Sekunde
-//Schusszähler
-  int schuss = 0;
-//CONFIG END ###########################################################
+int magazinCap = 40; // 40 Patronen im Magazin
+// MAgazinwechsel
+unsigned int magazinWechsel = 5000; // Zeit in Millisekunden (1000 = 1 Sekunde)
+// Schussfrequenz in Millisekunden (1000 = 1 Sekunde)
+const int interval = 100; // 10 mal in der Sekunde
+// Schusszähler
+int schuss = 0; // aktuelle Schussanzahl
+// Glockenkapazitaet
+int glockenKapazitaet = 200; // 200 Patronen aus einer Glocke
+int glockenKapazitaetCounter = 0;  // aktuelle Glockenkapazitaet
+int glockenDelay = 100000; // 100 Sekunden (1000 = 1 Sekunde)
+// CONFIG END ###########################################################
 
-void setup() {
-  // put your setup code here, to run once:
-  pinMode(KloppferPin, OUTPUT);
+void setup()
+{
+    // put your setup code here, to run once:
+    pinMode(KloppferPin, OUTPUT);
+    Serial.begin(9600);
 };
 
-void loop() {
-
-if(schuss <= magazinCap){
-  digitalWrite(KloppferPin, HIGH);
-  delay(interval);
-  digitalWrite(KloppferPin, LOW);
-  delay(20);
-  schuss++;
-}else{
-  schuss = 0;
-  delay(magazinWechsel);
-}
-}
-
+void loop()
+{
+    while (glockenKapazitaetCounter <= glockenKapazitaet) {
+        if (schuss <= magazinCap) {
+            digitalWrite(KloppferPin, HIGH);
+            delay(interval);
+            digitalWrite(KloppferPin, LOW);
+            delay(50);
+            schuss++;
+            glockenKapazitaetCounter++;
+        } else {
+            schuss = 0;
+            delay(magazinWechsel);
+        }
+        Serial.print("\t");
+        Serial.print("Glocke: ");
+        Serial.print(glockenKapazitaetCounter);
+        Serial.print("\t");
+        Serial.print("Schuss: ");
+        Serial.println(schuss);
+    }
+    delay(glockenDelay);
+    glockenKapazitaetCounter = 0;
+    schuss = 0;
+};
